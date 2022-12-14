fe7729532cdc876828734a4b7a8459aa8a35719a
Dackss <thomas.geffroy@outlook.com>
2022-12-14 14:40:39 +0100

    Ajouts

--- src/main.py
+++ src/main.py
@@ -12,11 +12,24 @@ class Calculator:
         self.window = pygame.display.set_mode((self.width, self.height))
         pygame.display.set_caption(self.title)
 
+    def text_test(self, text, font, color, x, y):
+        text = font.render(text, True, color)
+        self.window.blit(text, (x, y))
 
-fenetre = Calculator(800, 600, "CalculaTreece")
-running = True
+    def run(self):
+        running = True
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+            self.window.fill((255, 255, 255))
+            pygame.display.update()
+        pygame.quit()
+        sys.exit()
 
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+
+if __name__ == '__main__':
+    calc = Calculator(800, 600, 'Calculator')
+    calc.run()
+    calc.text_test('Hello World', pygame.font.SysFont('Arial', 20), (0, 0, 0), 0, 0)
+
