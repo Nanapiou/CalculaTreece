b66810caedcc642448a234e01566723a3fc4c76a
Dackss <thomas.geffroy@outlook.com>
2022-12-16 11:57:20 +0100

    Animations + Print de la valeur du bouton pressé dans la console

--- src/app.py
+++ src/app.py
@@ -41,65 +41,92 @@ class App:
     def button_number_1(self):
         button_1 = Button("1", 100, 75, (0, 350), self.screen)
         button_1.draw()
+        if button_1.check_click():
+            print("1")
 
     def button_number_2(self):
         button_2 = Button("2", 100, 75, (100, 350), self.screen)
         button_2.draw()
+        if button_2.check_click():
+            print("2")
 
     def button_number_3(self):
         button_3 = Button("3", 100, 75, (200, 350), self.screen)
         button_3.draw()
+        if button_3.check_click():
+            print("3")
 
     def button_number_4(self):
         button_4 = Button("4", 100, 75, (0, 275), self.screen)
         button_4.draw()
+        if button_4.check_click():
+            print("4")
 
     def button_number_5(self):
         button_5 = Button("5", 100, 75, (100, 275), self.screen)
         button_5.draw()
+        if button_5.check_click():
+            print("5")
 
     def button_number_6(self):
         button_6 = Button("6", 100, 75, (200, 275), self.screen)
         button_6.draw()
+        if button_6.check_click():
+            print("6")
 
     def button_number_7(self):
         button_7 = Button("7", 100, 75, (0, 200), self.screen)
         button_7.draw()
+        if button_7.check_click():
+            print("7")
 
     def button_number_8(self):
         button_8 = Button("8", 100, 75, (100, 200), self.screen)
         button_8.draw()
+        if button_8.check_click():
+            print("8")
 
     def button_number_9(self):
         button_9 = Button("9", 100, 75, (200, 200), self.screen)
         button_9.draw()
+        if button_9.check_click():
+            print("9")
 
     def button_operation_add(self):
         button_add = Button("+", 100, 75, (300, 350), self.screen)
         button_add.draw()
+        if button_add.check_click():
+            print("+")
 
     def button_operation_sub(self):
         button_sub = Button("-", 100, 75, (300, 275), self.screen)
         button_sub.draw()
+        if button_sub.check_click():
+            print("-")
 
     def button_operation_mul(self):
         button_mul = Button("*", 100, 75, (300, 200), self.screen)
         button_mul.draw()
+        if button_mul.check_click():
+            print("*")
 
     def button_operation_div(self):
         button_div = Button("/", 100, 75, (300, 125), self.screen)
         button_div.draw()
-        button_div.check_click()
+        if button_div.check_click():
+            print("/")
 
     def button_operation_equal(self):
         button_equal = Button("=", 100, 75, (300, 425), self.screen)
         button_equal.draw_equal_button()
-        button_equal.check_click()
+        if button_equal.check_click():
+            print("=")
 
     def button_operation_clear(self):
         button_clear = Button("C", 100, 75, (200, 125), self.screen)
         button_clear.draw()
-        button_clear.check_click()
+        if button_clear.check_click():
+            print("C")
 
     def run(self):
         """
@@ -148,7 +175,7 @@ class Button:
             if pygame.mouse.get_pressed()[0]:
                 pygame.draw.rect(self.screen, 'Black', self.top_rect, 1)
                 self.screen.blit(self.text_surf, self.text_rect)
-                print('Number pressed')
+                return 'Number Pressed'
             else:
                 pygame.draw.rect(self.screen, 'Grey', self.top_rect, 0)
                 self.screen.blit(self.text_surf, self.text_rect)
@@ -168,12 +195,10 @@ class Button:
         mouse_pos = pygame.mouse.get_pos()  # get the mouse position
         if self.top_rect.collidepoint(mouse_pos):  # check if the mouse position is over the button
             if pygame.mouse.get_pressed()[0]:
-                print("Clicked")
                 self.pressed = True
                 return True
             else:
                 if self.pressed:
-                    print("Button pressed")
                     self.pressed = False
 
 
