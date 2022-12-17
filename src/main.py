24a31596df5c08a6e3f8189e4a8e7680f7303ded
Xecozz <94707887+Xecozz@users.noreply.github.com>
2022-12-17 16:29:47 +0100

    Change width/height of calculator for more space

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ import pygame
 from src.Graphique.app import App
 
 pygame.init()
-screen = pygame.display.set_mode((415, 515))
+screen = pygame.display.set_mode((425, 520))
 gui_font = pygame.font.Font(None, 30)
 app = App(screen)
 app.run()
