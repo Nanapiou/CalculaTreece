4982ccf51acd67230d58b7e24bf0bca605ade4b5
Nanapiou <nana.juniot@gmail.com>
2022-12-18 15:51:26 +0100

    New icon bis

--- src/Graphique/app.py
+++ src/Graphique/app.py
@@ -151,7 +151,7 @@ class App:
         pygame.display.set_caption(self.title)
 
         # Set the icon
-        self.icon = pygame.image.load("Graphique/Assets/icon.png")
+        self.icon = pygame.image.load("Graphique/Assets/treeIcon2.png")
         pygame.display.set_icon(self.icon)
 
         # Set the running flag to True
