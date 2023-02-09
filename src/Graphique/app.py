8a7ca449032dfd64b383dbcb0f3d4a8be6cd7496
Dacksss <thomas.geffroy@outlook.com>
2023-02-09 13:25:54 +0100

    Revert "Handling syntax error"
    This reverts commit fafa574ff923af4cd4d45bc1f071932485138eff.

--- src/Graphique/app.py
+++ src/Graphique/app.py
@@ -168,18 +168,18 @@ class App:
 
     def draw_tree(self):
         """
-        Draw the tree
-        """
+            Draw the tree
+            """
+        # Clear the screen
+        self.screen.fill(self.bg_color)
+
         # Get the expression from the text box
         expression = self.text_box.text
 
         # Convert the expression into a tree
-        try:
-            lis = infix_automaton.build(expression)
-            clean_list_to_infix(lis)
-            tree = infix_list_to_tree(lis)
-        except SyntaxError:
-            return self.text_box.write_value('Error')
+        lis = infix_automaton.build(expression)
+        clean_list_to_infix(lis)
+        tree = infix_list_to_tree(lis)
 
         # Then draw using the method
         tree.draw(Turtle())
