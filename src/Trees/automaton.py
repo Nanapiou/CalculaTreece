0c187761e13952a0dac55dd9f42f56d9ed0d2677
Dackss <thomas.geffroy@outlook.com>
2023-03-15 22:22:32 +0100

    Bug fix

--- src/Trees/automaton.py
+++ src/Trees/automaton.py
@@ -159,7 +159,8 @@ infix_states: States = [
     #  1
     {  # Numbers
         '': (0, lambda old, _: (int(old) if old.isdigit() else float(old) if is_float(old) else old, '*')),
-        '×': (2, lambda old, _: int(old) if old.isdigit() else float(old) if is_float(old) else old),
+        '×' : (2, lambda old, _: int(old) if old.isdigit() else float(old) if is_float(old) else old),
+        '*' : (2, lambda old, _: int(old) if old.isdigit() else float(old) if is_float(old) else old),
         '÷': (18, lambda old, _: int(old) if old.isdigit() else float(old) if is_float(old) else old),
         '-': (6, lambda old, _: int(old) if old.isdigit() else float(old) if is_float(old) else old),
         ':': (6, lambda old, _: int(old) if old.isdigit() else float(old) if is_float(old) else old),
